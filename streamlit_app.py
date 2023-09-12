c682bcd7fc533df369dc903b1c8dbb4c57e408a1
josephanthonymatan <71606523+josephanthonymatan@users.noreply.github.com>
2023-09-12 12:26:00 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas as pd
 
 streamlit.title("HELLO, WORLD!")
 streamlit.header("This is a header...")
@@ -8,3 +9,8 @@ streamlit.text("...and this is text.")
 streamlit.text("And this is more text")
 
 streamlit.text("We can add emojis which is funny 🥣 🥗 🐔 🥑🍞")
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
+

241ec3db368428e850e5b3a2faaa11c959f6a27f
josephanthonymatan <71606523+josephanthonymatan@users.noreply.github.com>
2023-09-12 12:41:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,12 +11,13 @@ streamlit.text("And this is more text")
 streamlit.text("We can add emojis which is funny 🥣 🥗 🐔 🥑🍞")
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-fruits_selected = my_fruit_list.set_index('Fruit')
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-streamlit.dataframe(fruits_to_show)
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+streamlit.dataframe(my_fruit_list)
 
 streamlit.text("Let's put a pick list here so they can pick the fruit they want to include")
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 
 

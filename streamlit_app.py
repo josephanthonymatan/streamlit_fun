d726e59d5b18e1b5446b353fbb6e71fe215e053d
josephanthonymatan <71606523+josephanthonymatan@users.noreply.github.com>
2023-09-12 12:24:32 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.header("This is a header...")
 streamlit.text("...and this is text.")
 
 streamlit.text("And this is more text")
+
+streamlit.text("We can add emojis which is funny 🥣 🥗 🐔 🥑🍞")
